298d404e10dc4c2cee68ccbce12ed21e383cc7e1
Iswenzz <suxlolz1528@gmail.com>
2020-09-15 14:31:19 +0200

    Added webpack & npm configs

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	parser: "postcss-safe-parser",
+	plugins: {
+		"postcss-preset-env": {},
+		"postcss-font-magician": {},
+		"postcss-flexbugs-fixes": {},
+		"cssnano": {}
+	}
+};
