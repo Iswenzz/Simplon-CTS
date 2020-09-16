60ccf95bdf8145bed16d65cff677f6732ee86ee5
Iswenzz <suxlolz1528@gmail.com>
2020-09-16 20:30:31 +0200

    ContactDAO: Started DAO pattern for Contact MVC

--- src/backend/php/dao/ContactDAO.php
+++ src/backend/php/dao/ContactDAO.php
@@ -1,8 +1,37 @@
 <?php
+require "../DatabaseFactory.php";
+require "../model/Contact.php";
 
 class ContactDAO
 {
 	public const SELECT_QUERY = "SELECT * from Contact";
 
-	
+	public function getAllContacts(): array
+	{
+		$contacts = [];
+		$stmt = DatabaseFactory::getConnection()->prepare($this->SELECT_QUERY);
+        
+		while ($row = $stmt->fetch())
+		{
+			$contact = new Contact($row["codeContact"], $row["nomContact"], 
+				$row["prenomContact"], $row["dateNaissanceContact"], $row["codePays"]);
+            $contacts[] = $contact;
+        }
+        return $contacts;
+	}
+
+	public function updateContact(Contact $contact): void
+	{
+		
+	}
+
+	public function deleteContact(Contact $contact): void
+	{
+		
+	}
+
+	public function addContact(Contact $contact): void
+	{
+		
+	}
 }
