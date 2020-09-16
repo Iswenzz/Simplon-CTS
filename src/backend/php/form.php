c15e3b498d3e347d9e5fb04267e21f912228371e
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-16 10:55:30 +0200

    lien formulaire - php

--- src/backend/php/form.php
+++ src/backend/php/form.php
@@ -1,8 +1,8 @@
 <?php
-    require_once("./database.php");
-    
-    try {
-        $DB = new Database("admin", "E.F.Codd");
-    } catch (PDOException $error) {
-        print_r($error);
-    }
+require_once("./database.php");
+
+try {
+    $DB = new Database("admin", "E.F.Codd");
+} catch (PDOException $error) {
+    print_r($error);
+}
