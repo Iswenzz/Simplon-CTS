128a28a38e66fd2948798e548826a1117fa3c856
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-16 11:26:12 +0200

    enlevé try catch redondant

--- src/backend/php/form.php
+++ src/backend/php/form.php
@@ -1,8 +1,4 @@
 <?php
 require_once("./database.php");
 
-try {
-    $DB = new Database("admin", "E.F.Codd");
-} catch (PDOException $error) {
-    print_r($error);
-}
+$DB = new Database("admin", "E.F.Codd");
