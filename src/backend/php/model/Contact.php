a15e39db23faea19571cdc6ef8ef4dd15b01e0bf
Iswenzz <suxlolz1528@gmail.com>
2020-09-16 23:42:30 +0200

    Contact: Commented getters/setters

--- src/backend/php/model/Contact.php
+++ src/backend/php/model/Contact.php
@@ -11,6 +11,10 @@ class Contact
 
 	private ContactController $controller;
 
+	/**
+	 * Initailize a new Contact object.
+	 * @todo
+	 */
 	public function __construct(int $code, string $nom, string $prenom, 
 		DateTime $dateNaissance, int $codePays)
 	{
@@ -22,18 +26,93 @@ class Contact
 		$this->controller = new ContactController($this, new ContactView($this));
 	}
 
+	/**
+	 * Get the Contact controller instance.
+	 */
 	public function getController(): ContactController
 	{
 		return $this->controller;
 	}
 
+	/**
+	 * Get the value of code.
+	 */
 	public function getCode(): int
 	{
 		return $this->code;
 	}
 
+	/**
+	 * Set the value of code.
+	 */
 	public function setCode($code): void
 	{
 		$this->code = $code;
+	}
+
+	/**
+	 * Get the value of nom.
+	 */ 
+	public function getNom()
+	{
+		return $this->nom;
+	}
+
+	/**
+	 * Set the value of nom.
+	 */ 
+	public function setNom($nom)
+	{
+		$this->nom = $nom;
+
+		return $this;
+	}
+
+	/**
+	 * Get the value of prenom.
+	 */ 
+	public function getPrenom()
+	{
+		return $this->prenom;
+	}
+
+	/**
+	 * Set the value of prenom.
+	 */ 
+	public function setPrenom($prenom)
+	{
+		$this->prenom = $prenom;
+	}
+
+	/**
+	 * Get the value of dateNaissance.
+	 */ 
+	public function getDateNaissance()
+	{
+		return $this->dateNaissance;
+	}
+
+	/**
+	 * Set the value of dateNaissance.
+	 */ 
+	public function setDateNaissance($dateNaissance)
+	{
+		$this->dateNaissance = $dateNaissance;
+	}
+
+	/**
+	 * Get the value of codePays.
+	 */ 
+	public function getCodePays()
+	{
+		return $this->codePays;
+	}
+
+	/**
+	 * Set the value of codePays.
+	 */ 
+	public function setCodePays($codePays)
+	{
+		$this->codePays = $codePays;
 	}
 }
