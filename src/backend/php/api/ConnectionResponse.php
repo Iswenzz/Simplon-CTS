bbf340c39ac0a11243b392c106e6d380ce294324
Iswenzz <suxlolz1528@gmail.com>
2020-09-21 16:54:12 +0200

    ConnectionController: login req

---
+++ src/backend/php/api/ConnectionResponse.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . "/Response.php";
+require_once __DIR__ . "/../model/Admin.php";
+
+class ConnectionResponse extends Response
+{
+    public function __construct(bool $success = false, int $httpCode = 500, 
+        string $message = "Connexion non exécutée") 
+    {
+        parent::__construct($success, $httpCode, $message);
+    }
+    
+    /**
+     * Sends the prepared response to the output as JSON,
+     * with the HTTP code sent in an HTTP header.
+     *
+     * @param Admin $admin 
+     * @return void
+     */
+    public function send($admin)
+    {
+        header('Content-Type: application/json');
+        http_response_code($this->getHttpCode());
+        $return = json_encode(
+            [
+                "success" => $this->getSuccess(),
+                "message" => $this->getMessage(),
+                "key" => urlencode($admin->getApiKey()),
+                "user" => urlencode($admin->getEmail())
+            ]
+        );
+        echo $return;
+    }
+}
