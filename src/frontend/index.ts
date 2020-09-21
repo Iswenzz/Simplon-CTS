81a5c3cb9757a0759f769fad60ab610983a4367a
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-21 11:38:57 +0200

    (front) bugfix : dropdown reste mtn ouvert si clic

--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -7,7 +7,10 @@ import "./logout";
 document.addEventListener("DOMContentLoaded", () => {
 	const trigger = document.getElementById("login");
 	M.Dropdown.init(trigger, {
+		// appears below the trigger
 		coverTrigger: false,
+		// not closing when clicked
+		closeOnClick: false,
 		// lazy loading
 		onOpenStart: () => {
 			import("./login");
