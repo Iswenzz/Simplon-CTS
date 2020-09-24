f8cbc53171e3d18a9520881a849963081c02b796
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-24 16:51:53 +0200

    repo agent ❌

--- src/backend/php/api/AgentAPI.php
+++ src/backend/php/api/AgentAPI.php
@@ -11,105 +11,126 @@ require_once __DIR__ . "/../dao/AgentDAO.php";
  */
 class AgentAPI extends Controller
 {
-	private static ?AgentAPI $instance = null;
+    private static ?AgentAPI $instance = null;
 
-	/**
-	 * Agent singleton instance.
-	 */
-	private function __construct()
-	{
-		DAOFactory::registerDAO(AgentDAO::class);
-		$this->dao = DAOFactory::getDAO(AgentDAO::class);
-		$this->res = new Response();
-	}
-
-	/**
-	 * Get the singleton instance.
-	 */
-	public static function getInstance(): ?AgentAPI
-	{
-		if (!AgentAPI::$instance)
-			AgentAPI::$instance = new AgentAPI();
-		return AgentAPI::$instance;
-	}
-
-	/**
-	 * Get a specific Agent.
-	 */
-	private function get(): Response
-	{
-		/**
-		 * @var AgentDAO $dao
-		 * @var Agent $agent
-		 */
-		$dao = $this->dao;
-		$agent = $dao->getAgent($this->req->code);
-		return $this->res->prepare(Response::OK, true,
-			"Query successful", $agent);
-	}
-
-	/**
-	 * Get all Agents.
-	 */
-	private function getAll(): Response
-	{
-		/**
-		 * @var AgentDAO $dao
-		 */
-		$dao = $this->dao;
-		$Agents = $dao->getAllAgents();
-		return $this->res->prepare(Response::OK, true,
-            "Query successful", $Agents);
-	}
-
-	/**
-	 * Update a specific Agent.
-	 */
-	private function update(): Response
+    /**
+     * Agent singleton instance.
+     */
+    private function __construct()
     {
-		/**
-		 * @var AgentDAO $dao
-		 * @var Agent $Agent
-		 */
-		$dao = $this->dao;
-		$deserializer = new Deserializer(Agent::class, $this->req->Agent);
-		$Agent = $deserializer->deserialize();
-		$dao->updateAgent($Agent);
-		return $this->res->prepare(Response::OK, true,
-			"Query successful", $deserializer->deserialize());
-	}
+        DAOFactory::registerDAO(AgentDAO::class);
+        $this->dao = DAOFactory::getDAO(AgentDAO::class);
+        $this->res = new Response();
+    }
 
-	/**
-	 * Delete a specific Agent.
-	 */
-	private function delete(): Response
-	{
-		/**
-		 * @var AgentDAO $dao
-		 * @var Agent $Agent
-		 */
-		$dao = $this->dao;
-		$deserializer = new Deserializer(Agent::class, $this->req->Agent);
-		$Agent = $deserializer->deserialize();
-		$dao->deleteAgent($Agent);
-		return $this->res->prepare(Response::OK, true,
-			"Query successful", $deserializer->deserialize());
-	}
+    /**
+     * Get the singleton instance.
+     */
+    public static function getInstance(): ?AgentAPI
+    {
+        if (!AgentAPI::$instance) {
+            AgentAPI::$instance = new AgentAPI();
+        }
+        return AgentAPI::$instance;
+    }
 
-	/**
-	 * Prepare the request response.
-	 */
-	public function response(): Response
-	{
-		$requestBody = file_get_contents('php://input');
-		if (!$requestBody) // empty request
-			return $this->res->prepare(Response::BAD_REQUEST, false,
-				"Mauvaise syntaxe de requête / paramètres manquants :(");
-		$requestBody = json_decode($requestBody);
-		$this->req = $requestBody;
+    /**
+     * Get a specific Agent.
+     */
+    private function get(): Response
+    {
+        /**
+         * @var AgentDAO $dao
+         * @var Agent $agent
+         */
+        $dao = $this->dao;
+        $agent = $dao->getAgent($this->req->code);
+        return $this->res->prepare(
+            Response::OK,
+            true,
+            "Get successful",
+            $agent
+        );
+    }
 
-		// call the right response callback
-		return call_user_func([$this, $requestBody->method]);
-	}
+    /**
+     * Get all Agents.
+     */
+    private function getAll(): Response
+    {
+        /**
+         * @var AgentDAO $dao
+         */
+        $dao = $this->dao;
+        $Agents = $dao->getAllAgents();
+        return $this->res->prepare(
+            Response::OK,
+            true,
+            "GetAll successful",
+            $Agents
+        );
+    }
+
+    /**
+     * Update a specific Agent.
+     */
+    private function update(): Response
+    {
+        /**
+         * @var AgentDAO $dao
+         * @var Agent $Agent
+         */
+        $dao = $this->dao;
+        $deserializer = new Deserializer(Agent::class, $this->req->Agent);
+        $Agent = $deserializer->deserialize();
+        $dao->updateAgent($Agent);
+        return $this->res->prepare(
+            Response::OK,
+            true,
+            "Update successful",
+            $deserializer->deserialize()
+        );
+    }
+
+    /**
+     * Delete a specific Agent.
+     */
+    private function delete(): Response
+    {//TODO à débugguer : n'efface pas dans la BDD
+        /**
+         * @var AgentDAO $dao
+         * @var Agent $Agent
+         */
+        $dao = $this->dao;
+        $deserializer = new Deserializer(Agent::class, $this->req->agent);
+        $Agent = $deserializer->deserialize();
+        $dao->deleteAgent($Agent);
+        return $this->res->prepare(
+            Response::OK,
+            true,
+            "Delete successful",
+            $deserializer->deserialize()
+        );
+    }
+
+    /**
+     * Prepare the request response.
+     */
+    public function response(): Response
+    {
+        $requestBody = file_get_contents('php://input');
+        if (!$requestBody) { // empty request
+            return $this->res->prepare(
+                Response::BAD_REQUEST,
+                false,
+                "Mauvaise syntaxe de requête / paramètres manquants :("
+            );
+        }
+        $requestBody = json_decode($requestBody);
+        $this->req = $requestBody;
+
+        // call the right response callback
+        return call_user_func([$this, $requestBody->method]);
+    }
 }
 AgentAPI::getInstance()->response()->send();
--- src/backend/php/model/Agent.php
+++ src/backend/php/model/Agent.php
@@ -3,127 +3,132 @@ require_once __DIR__ . "/../controller/AgentController.php";
 
 class Agent implements JsonSerializable
 {
-	private ?int $code;
-	private string $nom;
-	private string $prenom;
-	private DateTime $dateNaissance;
-	private int $codePays;
+    private ?int $code;
+    private string $nom;
+    private string $prenom;
+    private DateTime $dateNaissance;
+    private int $codePays;
 
-	private AgentController $controller;
+    private AgentController $controller;
 
-	/**
-	 * Initialize a new Agent object.
-	 */
-	public function __construct(?int $code, string $nom, string $prenom, 
-		DateTime $dateNaissance, int $codePays)
-	{
-		$this->code = $code;
-		$this->nom = $nom;
-		$this->prenom = $prenom;
-		$this->dateNaissance = $dateNaissance;
-		$this->codePays = $codePays;
-		$this->controller = new AgentController($this, new AgentView($this));
-	}
+    /**
+     * Initialize a new Agent object.
+     */
+    public function __construct(
+        ?int $code = null,
+        string $nom = "",
+        string $prenom = "",
+        DateTime $dateNaissance = null,
+        int $codePays = 0
+    )
+    {
+        $this->code = $code;
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->dateNaissance = $dateNaissance ?? new DateTime();
+        $this->codePays = $codePays;
+        $this->controller = new AgentController($this, new AgentView($this));
+    }
 
-	/**
-	 * Get the Agent controller instance.
-	 */
-	public function getController(): AgentController
-	{
-		return $this->controller;
-	}
+    /**
+     * Get the Agent controller instance.
+     */
+    public function getController(): AgentController
+    {
+        return $this->controller;
+    }
 
-	/**
-	 * Get the value of code.
-	 */
-	public function getCode(): ?int
-	{
-		return $this->code;
-	}
+    /**
+     * Get the value of code.
+     */
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
 
-	/**
-	 * Set the value of code.
-	 */
-	public function setCode(?int $code): void
-	{
-		$this->code = $code;
-	}
+    /**
+     * Set the value of code.
+     */
+    public function setCode(?int $code): void
+    {
+        $this->code = $code;
+    }
 
-	/**
-	 * Get the value of nom.
-	 */ 
-	public function getNom(): string
-	{
-		return $this->nom;
-	}
+    /**
+     * Get the value of nom.
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
 
-	/**
-	 * Set the value of nom.
-	 */ 
-	public function setNom(string $nom): void
-	{
-		$this->nom = $nom;
-	}
+    /**
+     * Set the value of nom.
+     */
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
 
-	/**
-	 * Get the value of prenom.
-	 */ 
-	public function getPrenom(): string
-	{
-		return $this->prenom;
-	}
+    /**
+     * Get the value of prenom.
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
 
-	/**
-	 * Set the value of prenom.
-	 */ 
-	public function setPrenom(string $prenom): void
-	{
-		$this->prenom = $prenom;
-	}
+    /**
+     * Set the value of prenom.
+     */
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
 
-	/**
-	 * Get the value of dateNaissance.
-	 */ 
-	public function getDateNaissance(): DateTime
-	{
-		return $this->dateNaissance;
-	}
+    /**
+     * Get the value of dateNaissance.
+     */
+    public function getDateNaissance(): DateTime
+    {
+        return $this->dateNaissance;
+    }
 
-	/**
-	 * Set the value of dateNaissance.
-	 */ 
-	public function setDateNaissance(DateTime $dateNaissance): void
-	{
-		$this->dateNaissance = $dateNaissance;
-	}
+    /**
+     * Set the value of dateNaissance.
+     */
+    public function setDateNaissance(DateTime $dateNaissance): void
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
 
-	/**
-	 * Get the value of codePays
-	 */ 
-	public function getCodePays(): int
-	{
-		return $this->codePays;
-	}
+    /**
+     * Get the value of codePays
+     */
+    public function getCodePays(): int
+    {
+        return $this->codePays;
+    }
 
-	/**
-	 * Set the value of codePays
-	 */ 
-	public function setCodePays(int $codePays): void
-	{
-		$this->codePays = $codePays;
-	}
+    /**
+     * Set the value of codePays
+     */
+    public function setCodePays(int $codePays): void
+    {
+        $this->codePays = $codePays;
+    }
 
-	/**
-	 * Serialize the object.
-	 */
-	public function jsonSerialize(): array
-	{
-		return [
-			"code" => $this->getCode(),
-			"nom" => $this->getNom(),
-			"prenom" => $this->getPrenom(),
-			"dateNaissance" => $this->getDateNaissance()->format("Y-m-d"),
-			"codePays" => $this->getCodePays()
-		];
-	}
+    /**
+     * Serialize the object.
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            "code" => $this->getCode(),
+            "nom" => $this->getNom(),
+            "prenom" => $this->getPrenom(),
+            "dateNaissance" => $this->getDateNaissance()->format("Y-m-d"),
+            "codePays" => $this->getCodePays()
+        ];
+    }
 }
