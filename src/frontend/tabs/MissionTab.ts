029ce6779716b914a49f11ff64632db59236af9f
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-25 15:46:57 +0200

    (front / mission) màj des dates

--- src/frontend/tabs/MissionTab.ts
+++ src/frontend/tabs/MissionTab.ts
@@ -12,7 +12,8 @@ export default class MissionTab {
 	/**
 	 * The link with the API
 	 */
-	private repo: MissionRepository;
+	private missionRepo: MissionRepository;
+	// private paysRepo: PaysRepository;
 	/**
 	 * The HTML inputs (in the back-office)
 	 */
@@ -22,22 +23,28 @@ export default class MissionTab {
 	private description : HTMLTextAreaElement;
 	private dateDebut : HTMLInputElement;
 	private dateFin : HTMLInputElement;
-	private type : HTMLParagraphElement;
+	private typeMission : HTMLParagraphElement; // join avec MissionType & Spécialité
+	private pays : HTMLSelectElement;
+	private paysDesc : HTMLParagraphElement; // join avec Abrite
+	private planqueDesc : HTMLParagraphElement; // join avec Abrite
+	private planque : HTMLParagraphElement; // join avec Abrite
 
-	public constructor(repo: MissionRepository, inputs: HTMLElement[], mission: Mission | null = null) {
+	public constructor(missionRepo: MissionRepository, inputs: HTMLElement[], mission: Mission | null = null) {
 		this.inputs = [];
 		inputs.forEach((input) => {
 			this.inputs.push(input);
 		});
 		this.mission = mission;
-		this.repo = repo;
+		this.missionRepo = missionRepo;
 
 		this.titre = document.getElementById("mission-details-name") as HTMLInputElement;
 		this.description = document.getElementById("mission-details-desc") as HTMLTextAreaElement;
+		this.dateDebut = document.getElementById("missions-details-date-start") as HTMLInputElement;
+		this.dateFin = document.getElementById("missions-details-date-end") as HTMLInputElement;
 	}
 
 	public init(): void {
-		this.repo.listAll();
+		this.missionRepo.listAll();
 
 		this.inputs.forEach((input) => {
 			// TODO on leaving the input, updates the model's relevant values
@@ -59,7 +66,7 @@ export default class MissionTab {
 			// handling the results
 			if (res) {
 				const missionModel = new Mission(null, input.getInput().value);
-				this.setMission(missionModel);
+				this.updateMission(missionModel);
 				console.log("Nouvelle mission : " + missionModel.format());
 			}
 		});
@@ -68,18 +75,28 @@ export default class MissionTab {
 	// public reset();
 
 	/**
-	 * Updates the detailed view of the missions
-	 * @param mission - model of the Mission
+	 * Updates the inner model of the mission
+	 * @param mission - model of the mission
 	 */
 	public setMission(mission: Mission): void {
 		this.mission = mission;
-
-		this.titre.value = this.mission.getTitre();
-		this.description.value = this.mission.getDescription();
-		this.dateDebut.value = dayjs(this.mission.getDateDebut()).format("YYYY-MM-DD");
-		this.dateFin.value = dayjs(this.mission.getDateFin()).format("YYYY-MM-DD");
 	}
+	
 	public getMission(): Mission {
 		return this.mission;
+	}
+
+	/**
+	 * Updates the detailed view of the missions
+	 * @param mission - model of the Mission
+	 */
+	public updateMission(mission: Mission): void {
+		this.setMission(mission);
+		this.titre.value = this.mission.getTitre();
+		this.description.value = this.mission.getDescription();
+		const dD = this.mission.getDateDebut() ? dayjs(this.mission.getDateDebut()) : dayjs();
+		this.dateDebut.value =  dD.format("YYYY-MM-DD");
+		const dF = this.mission.getDateFin() ? dayjs(this.mission.getDateFin()) : dayjs();
+		this.dateFin.value = dF.format("YYYY-MM-DD");
 	}
 }
