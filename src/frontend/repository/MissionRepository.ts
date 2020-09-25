1141108e0b4ca6ec1e12c8fa91a3bd217d37a376
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-25 09:29:38 +0200

    corrigé chemin import deserializer

--- src/frontend/repository/MissionRepository.ts
+++ src/frontend/repository/MissionRepository.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import DeleteButton from "../component/DeleteButton";
-import Deserializer from "../Deserializer";
+import Deserializer from "../util/Deserializer";
 import Mission from "../model/Mission";
 import Repository from "./Repository";
 
--- src/frontend/repository/PlanqueRepository.ts
+++ src/frontend/repository/PlanqueRepository.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 import DeleteButton from "../component/DeleteButton";
-import Deserializer from "../Deserializer";
+import Deserializer from "../util/Deserializer";
 import Planque from "../model/Planque";
 import Repository from "./Repository";
 
