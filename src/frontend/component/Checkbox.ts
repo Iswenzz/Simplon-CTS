a2fbf8e73f12d65dd3b03a433f87ccfa39b2d0d0
Julie Souchet <julie.souchet.simplon@gmail.com>
2020-09-25 21:37:55 +0200

    enregistre plus d'informations

--- src/frontend/component/Checkbox.ts
+++ src/frontend/component/Checkbox.ts
@@ -2,8 +2,17 @@ export default class Checkbox {
 	private input: HTMLInputElement;
 	private label: HTMLLabelElement;
 	private text: HTMLSpanElement;
+	private codeTarget: number | null; // ex: mission qu'on est en train de modifier
+	private codeProperty: number; // ex: un agent qu'on affecte à la mission
 
-	public constructor(label: string, value: string) {
+	/**
+	 * 
+	 * @param label - label (texte) pour la checkbox
+	 * @param value - value (code) pour la chekcbox
+	 * @param codeTarget - code pour la cible (ex: la mission qu'on modifie) - peut être inconnu
+	 * @param codeProperty - code pour la propriété représentée par la checkbox (ex: un agent qu'on affecte à la mission)
+	 */
+	public constructor(label: string, value: string, codeTarget: number | null, codeProperty: number) {
 		this.label = document.createElement("label");
 		this.input = document.createElement("input");
 		this.label.append(this.input);
@@ -13,6 +22,9 @@ export default class Checkbox {
 		this.text = document.createElement("span");
 		this.label.append(this.text);
 		this.text.innerText = label;
+
+		this.codeTarget = codeTarget;
+		this.codeProperty = codeProperty;
 	}
 
 	public getContainer(): HTMLLabelElement {
@@ -25,5 +37,13 @@ export default class Checkbox {
 
 	public getInput(): HTMLInputElement {
 		return this.input;
+	}
+
+	public getKey(): {codeTarget: number | null, codeProperty: number} {
+		return {codeProperty: this.codeProperty, codeTarget: this.codeTarget};
+	}
+
+	public setCodeTarget(c: number): void {
+		this.codeTarget = c;
 	}
 }
